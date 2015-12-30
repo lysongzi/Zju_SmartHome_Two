d6da8fb1d3e26aee5d3b563e4f376b30a5240b8f
lysongzZ_ <642048384@qq.com>
2015-12-30 19:10:02 +0800

    单品界面再更改

---
+++ Zju_SmartHomeV1/Zju_SmartHome/Classes/YSPatternNewViewController.h
@@ -0,0 +1,13 @@
+//
+//  YSPatternNewViewController.h
+//  Zju_SmartHome
+//
+//  Created by lysongzi on 15/12/30.
+//  Copyright © 2015年 GJY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSPatternNewViewController : UIViewController
+
+@end
